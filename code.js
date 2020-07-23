b5cc4637bb5fe0b51e64e26c5707f51ecfbc022e
ElizavetaPanasiuk <65709545+ElizavetaPanasiuk@users.noreply.github.com>
2020-07-23 14:31:06 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,71 @@
+let personImg = document.querySelector("#personImg");
+personImg.addEventListener("mouseover", function(){
+    this.src = "img/person11.png";
+});
+personImg.addEventListener("mouseout", function(){
+    this.src = "img/person1.png";
+});
+
+let pointsImg = document.querySelector("#pointsImg");
+pointsImg.addEventListener("mouseover", function () {
+    this.src = "img/points1.png";
+});
+pointsImg.addEventListener("mouseout", function(){
+    this.src = "img/points.png";
+});
+
+let pluspersonImg = document.querySelector("#pluspersonImg");
+pluspersonImg.addEventListener("mouseover", function () {
+    this.src = "img/plusperson1.png";
+});
+pluspersonImg.addEventListener("mouseout", function(){
+    this.src = "img/plusperson.png";
+});
+
+let phoneImg = document.querySelector("#phoneImg");
+phoneImg.addEventListener("mouseover", function () {
+    this.src = "img/phone1.png";
+});
+phoneImg.addEventListener("mouseout", function(){
+    this.src = "img/phone.png";
+});
+
+let cameraImg = document.querySelector("#cameraImg");
+cameraImg.addEventListener("mouseover", function () {
+    this.src = "img/camera1.png";
+});
+cameraImg.addEventListener("mouseout", function(){
+    this.src = "img/camera.png";
+});
+
+let infoImg = document.querySelector("#infoImg");
+infoImg.addEventListener("mouseover", function () {
+    this.src = "img/info2.png";
+});
+infoImg.addEventListener("mouseout", function(){
+    this.src = "img/info1.png";
+});
+
+let gifImg = document.querySelector("#gifImg");
+gifImg.addEventListener("mouseover", function () {
+    this.src = "img/gif1.png";
+});
+gifImg.addEventListener("mouseout", function(){
+    this.src = "img/gif.png";
+});
+
+let plusImg = document.querySelector("#plusImg");
+plusImg.addEventListener("mouseover", function () {
+    this.src = "img/plus1.png";
+});
+plusImg.addEventListener("mouseout", function(){
+    this.src = "img/plus.png";
+});
+
+let stickImg = document.querySelector("#stickImg");
+stickImg.addEventListener("mouseover", function () {
+    this.src = "img/stick1.png";
+});
+stickImg.addEventListener("mouseout", function(){
+    this.src = "img/stick.png";
+});
